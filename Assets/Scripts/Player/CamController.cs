42f79f2a50f7cd8d52213c23c5c292112b548f5c
anilas5235 <niklas-borchers@outlook.de>
2025-10-26 17:24:18 +0100

    feat: implemented a hud display for selected voxelId

--- Assets/Scripts/Player/CamController.cs
+++ Assets/Scripts/Player/CamController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace Test
+namespace Player
 {
     public class CamController : MonoBehaviour
     {
---
+++ Assets/Scripts/UI/VoxelSelect.cs
@@ -0,0 +1,40 @@
+﻿using Player;
+using Runtime.Engine.Voxels.Data;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class VoxelSelect : MonoBehaviour
+    {
+        public RawImage voxelImage;
+        public ushort voxelId;
+        
+        public VoxelEditor voxelEditor;
+        
+        private void OnEnable()
+        {
+            voxelEditor.OnVoxelIdChanged += OnVoxelIdChanged;
+            OnVoxelIdChanged(voxelEditor.voxelId);
+        }
+        
+        private void OnDisable()
+        {
+            voxelEditor.OnVoxelIdChanged -= OnVoxelIdChanged;
+        }
+        
+        private void OnVoxelIdChanged(ushort newVoxelId)
+        {
+            voxelId = newVoxelId;
+            if(VoxelDataImporter.Instance.VoxelRegistry.GetVoxelDefinition(voxelId, out VoxelDefinition definition))
+            {
+                Texture2D tex = definition.GetTexture(Direction.Forward);
+                voxelImage.texture = tex;
+            }
+            else
+            {
+                voxelImage.texture = null;
+            }
+        }
+    }
+}
