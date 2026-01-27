33d0d3ee5e112331c87b2d694e4fea10dd6bb110
anilas5235 <niklas-borchers@outlook.de>
2026-01-28 00:22:04 +0100

    feat: add mesh combining functionality and custom editor for ChunkBehaviour. semas like Mesh.Combine can´t work with VertexAttributeDescriptor

--- Assets/Runtime/Engine/Behaviour/ChunkBehaviour.cs
+++ Assets/Runtime/Engine/Behaviour/ChunkBehaviour.cs
@@ -1,7 +1,11 @@
 ﻿using System.Collections.Generic;
+using Runtime.Engine.Mesher;
 using Runtime.Engine.Settings;
+using Unity.Collections;
 using Unity.Mathematics;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace Runtime.Engine.Behaviour
 {
@@ -50,5 +54,67 @@ namespace Runtime.Engine.Behaviour
         }
 
         public ChunkPartition GetPartition(int y) => chunkPartitions[y];
+
+        public void CombineMeshes()
+        {
+            MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+            List<CombineInstance> combineInstances = new();
+            int totalVertexCount = 0;
+
+            for (int i = 0; i < meshFilters.Length; i++)
+            {
+                MeshFilter meshFilter = meshFilters[i];
+                if (meshFilter.gameObject == gameObject) continue;
+
+                Mesh mesh = meshFilter.sharedMesh;
+                if (!mesh || !mesh.isReadable || mesh.vertexCount < 2) continue;
+
+                totalVertexCount += mesh.vertexCount;
+                combineInstances.Add(new CombineInstance
+                {
+                    mesh = mesh,
+                    transform = transform.worldToLocalMatrix * meshFilter.transform.localToWorldMatrix,
+                    subMeshIndex = 0,
+                });
+
+                meshFilter.gameObject.SetActive(false);
+            }
+
+            if (combineInstances.Count == 0) return;
+
+            Mesh combinedMesh = new();
+            var vertexParams = new NativeArray<VertexAttributeDescriptor>(5, Allocator.Persistent)
+            {
+                // Int interpolation cause issues
+                [0] = new VertexAttributeDescriptor(VertexAttribute.Position),
+                [1] = new VertexAttributeDescriptor(VertexAttribute.Normal),
+                [2] = new VertexAttributeDescriptor(VertexAttribute.TexCoord0, VertexAttributeFormat.Float32, 4),
+                [3] = new VertexAttributeDescriptor(VertexAttribute.TexCoord1, VertexAttributeFormat.Float32, 4),
+                [4] = new VertexAttributeDescriptor(VertexAttribute.TexCoord2, VertexAttributeFormat.Float32, 4)
+            };
+            combinedMesh.SetVertexBufferParams(totalVertexCount, vertexParams);
+            
+            CombineInstance[] instances = combineInstances.ToArray();
+            combinedMesh.CombineMeshes(instances, true, false);
+            combinedMesh.RecalculateBounds();
+            gameObject.GetComponent<MeshFilter>().sharedMesh = combinedMesh;
+        }
+    }
+
+    [CustomEditor(typeof(ChunkBehaviour))]
+
+    public class ChunkBehaviourCustomEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            ChunkBehaviour chunkBehaviour = (ChunkBehaviour)target;
+            if (GUILayout.Button("Combine Meshes"))
+            {
+                chunkBehaviour.CombineMeshes();
+            }
+        }
+        
     }
 }
