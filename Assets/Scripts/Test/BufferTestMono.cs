e0a9cd685c732604d0aed835ca6e845afa45068f
anilas5235 <niklas-borchers@outlook.de>
2026-02-19 21:47:01 +0100

    feat: update vertex structure and add CustomVoxel shader for enhanced rendering capabilities

--- Assets/Scripts/Test/BufferTestMono.cs
+++ Assets/Scripts/Test/BufferTestMono.cs
@@ -1,9 +1,9 @@
 using System.Collections.Generic;
-using Unity.Collections;
+using System.Runtime.InteropServices;
+using Runtime.Engine.Utils.Extensions;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Rendering;
-using Random = UnityEngine.Random;
 
 namespace Test
 {
@@ -14,16 +14,17 @@ namespace Test
         {
             new VertexAttributeDescriptor(VertexAttribute.Position),
             new VertexAttributeDescriptor(VertexAttribute.Normal),
-            new VertexAttributeDescriptor(VertexAttribute.TexCoord0, VertexAttributeFormat.Float16, 4),
+            new VertexAttributeDescriptor(VertexAttribute.TexCoord0, VertexAttributeFormat.UInt8, 4),
             new VertexAttributeDescriptor(VertexAttribute.TexCoord1, VertexAttributeFormat.Float16, 4),
             new VertexAttributeDescriptor(VertexAttribute.TexCoord2, VertexAttributeFormat.Float16, 4)
         };
 
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
         public struct Vertex
         {
             public float3 Position; // xyz = position, w = 0 (unused could hold extra data)
             public float3 Normal; // xyz = normal, w = 0 (unused could hold extra data)
-            public half4 UV0; // xy = Sized UV for texture atlas, zw = normalized UV for shader sampling
+            public uint UV0; // xy = Sized UV for texture atlas, zw = normalized UV for shader sampling
             public half4 UV1; // x = texture ID, y = depth fade factor, z = unused, w = sunlight level
             public half4 AO; // xyzw = AO values 
         }
@@ -42,7 +43,7 @@ namespace Test
                 {
                     Position = new float3(1f, 0.0f, 1.0f),
                     Normal = new float3(1.0f, 0.0f, 0.0f),
-                    UV0 = (half4)float4.zero,
+                    UV0 = uint.MaxValue,
                     UV1 = (half4)new float4(0.0f, 0f, 0.0f, 15f),
                     AO = (half4)new float4(1.0f, 1.0f, 1.0f, 1f)
                 },
@@ -50,7 +51,6 @@ namespace Test
                 {
                     Position = new float3(0.0f, 0.0f, 0.0f),
                     Normal = new float3(-1.0f, 0.0f, 0.0f),
-                    UV0 = (half4)float4.zero,
                     UV1 = (half4)new float4(0.0f, 0f, 0.0f, 15f),
                     AO = (half4)new float4(0.0f, 1.0f, 1.0f, 1f)
                 },
@@ -58,7 +58,6 @@ namespace Test
                 {
                     Position = new float3(0.0f, 1.0f, 0.0f),
                     Normal = new float3(0.0f, 1.0f, 0.0f),
-                    UV0 = (half4)float4.zero,
                     UV1 = (half4)new float4(2.0f, 0f, 0.0f, 15f),
                     AO = (half4)new float4(1.0f, 1.0f, 1.0f, 1f)
                 },
@@ -66,7 +65,6 @@ namespace Test
                 {
                     Position = new float3(0.0f, 0.0f, 1.0f),
                     Normal = new float3(0.0f, -1.0f, 0.0f),
-                    UV0 = (half4)float4.zero,
                     UV1 = (half4)new float4(2.0f, 0f, 0.0f, 15f),
                     AO = (half4)new float4(1.0f, 1.0f, 1.0f, 1f)
                 },
@@ -74,7 +72,6 @@ namespace Test
                 {
                     Position = new float3(0.0f, 0.0f, 1.0f),
                     Normal = new float3(0.0f, 0.0f, 1.0f),
-                    UV0 = (half4)float4.zero,
                     UV1 = (half4)new float4(4.0f, 0f, 0.0f, 15f),
                     AO = (half4)new float4(1.0f, 1.0f, 1.0f, 1f)
                 },
@@ -82,8 +79,57 @@ namespace Test
                 {
                     Position = new float3(1.0f, 0.0f, 0.0f),
                     Normal = new float3(0.0f, 0.0f, -1.0f),
-                    UV0 = (half4)float4.zero,
                     UV1 = (half4)new float4(4.0f, 0f, 0.0f, 15f),
+                    AO = (half4)new float4(1.0f, 1.0f, 1.0f, 1f)
+                },
+
+                new Vertex
+                {
+                    Position = new float3(2f, 0.0f, 0.0f),
+                    Normal = new float3(1.0f, 0.0f, 0.0f),
+                    UV1 = (half4)new float4(0.0f, 0f, 0.0f, 15f),
+                    AO = (half4)new float4(1.0f, 1.0f, 1.0f, 1f)
+                },
+                new Vertex
+                {
+                    Position = new float3(2.0f, 0.0f, 0.0f),
+                    Normal = new float3(-1.0f, 0.0f, 0.0f),
+                    UV1 = (half4)new float4(0.0f, 0f, 0.0f, 15f),
+                    AO = (half4)new float4(0.0f, 1.0f, 1.0f, 1f)
+                },
+                new Vertex
+                {
+                    Position = new float3(2.0f, 0.0f, 0.0f),
+                    Normal = new float3(0.0f, 1.0f, 0.0f),
+                    UV1 = (half4)new float4(2.0f, 0f, 0.0f, 15f),
+                    AO = (half4)new float4(1.0f, 1.0f, 1.0f, 1f)
+                },
+                new Vertex
+                {
+                    Position = new float3(2.0f, 0.0f, 0.0f),
+                    Normal = new float3(0.0f, -1.0f, 0.0f),
+                    UV1 = (half4)new float4(2.0f, 0f, 0.0f, 15f),
+                    AO = (half4)new float4(1.0f, 1.0f, 1.0f, 1f)
+                },
+                new Vertex
+                {
+                    Position = new float3(2.0f, 0.0f, 0.0f),
+                    Normal = new float3(0.0f, 0.0f, 1.0f),
+                    UV1 = (half4)new float4(4.0f, 0f, 0.0f, 15f),
+                    AO = (half4)new float4(1.0f, 1.0f, 1.0f, 1f)
+                },
+                new Vertex
+                {
+                    Position = new float3(2.0f, 0.0f, 0.0f),
+                    Normal = new float3(0.0f, 0.0f, -1.0f),
+                    UV1 = (half4)new float4(4.0f, 0f, 0.0f, 15f),
+                    AO = (half4)new float4(1.0f, 1.0f, 1.0f, 1f)
+                },
+                new Vertex
+                {
+                    Position = new float3(5.0f, 0.0f, 0.0f),
+                    Normal = new float3(0.0f, 1.0f, -1.0f).Normalized(),
+                    UV1 = (half4)new float4(8.0f, 0f, 0.0f, 15f),
                     AO = (half4)new float4(1.0f, 1.0f, 1.0f, 1f)
                 }
             };
